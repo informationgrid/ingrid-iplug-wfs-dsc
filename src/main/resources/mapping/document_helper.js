331d2973e9113744f8892ac621b2818da902d8a4
Toan Tran <khanh.toan.tran@wemove.com>
2022-03-18 15:02:24 +0100

    Update wadaba script for baw_wsv new layout

--- src/main/resources/mapping/document_helper.js
+++ src/main/resources/mapping/document_helper.js
@@ -99,3 +99,68 @@ function addDetailDetailsWrapper(parent) {
 	result = addOutputWithAttributes(result, "div", ["class"], ["box__content ob-container"]);
 	return result;
 }
+
+/* New portal layout */ 
+
+// add elements/styles for correct display in portal (header)
+function addDetailHeaderWrapperNewLayout(parent) {
+    var result = addOutputWithAttributes(parent, "div", ["class"], ["banner-noimage m-filter"]);
+    result = addOutputWithAttributes(result, "div", ["class", "style"], ["page-wrapper", "background-image: url('/decorations/layout/uvp/images/template/drops-subpage.svg');"]);
+    result = addOutputWithAttributes(result, "div", ["class"], ["row"]);
+    return result;
+}
+
+function addDetailHeaderWrapperNewLayoutBackSearch(parent) {
+    var result = addOutputWithAttributes(parent, "section", ["class"], ["xsmall-24 large-6 xlarge-6 columns"]);
+    var a = addOutputWithAttributes(result, "a", ["class", "href", "title"], ["helper icon", "/freitextsuche", "Alle Suchergebnisse"]);
+    addOutputWithAttributes(a, "span", ["class"], ["ic-ic-arrow-left"]);
+    var text = addOutputWithAttributes(a, "span", ["class"], ["text text-normal"]);
+    text.appendChild(document.createTextNode("Alle Suchergebnisse"));
+}
+
+function addDetailHeaderWrapperNewLayoutTitle(parent, title) {
+    var result = addOutputWithAttributes(parent, "section", ["class"], ["xsmall-24 large-18 xlarge-18 columns"]);
+    addOutput(result, "h2", title);
+}
+
+function addDetailHeaderWrapperNewLayoutDetailNavigation(parent, summary, detail) {
+    var result = addOutputWithAttributes(parent, "div", ["class"], ["xsmall-24 large-6 xlarge-6 columns"]);
+    result = addOutputWithAttributes(result, "div", ["class", "data-accordion", "data-allow-all-closed", "role"], ["accordion accordion-filter-group filter", "", "true", "tablist"]);
+    var filter = addOutputWithAttributes(result, "div", ["class", "data-accordion-item"], ["accordion-item accordion-item-filter-group", ""]);
+    var a = addOutputWithAttributes(filter, "a", ["class", "href", "role", "id", "aria-expanded", "aria-selected", "aria-controls"], ["accordion-title accordion-title-filter-group hide-for-large", "#", "tab", "detail-content-accordion-label", "false", "false", "detail-content-accordion"]);
+    addOutput(a, "span", "Inhalt");
+    var filterContent = addOutputWithAttributes(filter, "div", ["class", "data-tab-content", "aria-hidden", "role", "aria-labelledby", "id", "tabindex"], ["accordion-content filter-wrapper", "", "true", "tabpanel", "detail-content-accordion-label", "detail-content-accordion", "1"]);
+    var filterList = addOutputWithAttributes(filterContent, "ul", ["class", "data-accordion", "data-allow-all-closed", "role"], ["accordion filter-group nav-group", "", "true", "tablist"]);
+    
+    var filterEntry = addOutputWithAttributes(filterList, "li", ["class", "data-accordion-item"], ["accordion-item ", ""]);
+    var filterEntryHref = addOutputWithAttributes(filterEntry, "a", ["class", "href", "role", "id", "aria-expanded", "aria-selected", "aria-controls"], ["accordion-title js-anchor-target", "#detail_overview", "tab", "detail_overview-accordion-label", "false", "false", "detail_overview-accordion"]);
+    addOutput(filterEntryHref, "span", "Übersicht");
+    var filterEntrySub = addOutputWithAttributes(filterEntry, "div", ["class", "data-tab-content", "role", "id", "aria-hidden", "aria-labelledby"], ["accordion-content is-hidden", "", "tab", "detail_overview-accordion", "true", "detail_overview-accordion-label"]);
+    addOutputWithAttributes(filterEntrySub, "div", ["class"], ["boxes"]);
+    
+    if(summary) {
+        var filterEntry = addOutputWithAttributes(filterList, "li", ["class", "data-accordion-item"], ["accordion-item ", ""]);
+        var filterEntryHref = addOutputWithAttributes(filterEntry, "a", ["class", "href", "role", "id", "aria-expanded", "aria-selected", "aria-controls"], ["accordion-title js-anchor-target", "#detail_description", "tab", "detail_description-accordion-label", "false", "false", "detail_description-accordion"]);
+        addOutput(filterEntryHref, "span", "Beschreibung");
+        var filterEntrySub = addOutputWithAttributes(filterEntry, "div", ["class", "data-tab-content", "role", "id", "aria-hidden", "aria-labelledby"], ["accordion-content is-hidden", "", "tab", "detail_description-accordion", "true", "detail_description-accordion-label"]);
+        addOutputWithAttributes(filterEntrySub, "div", ["class"], ["boxes"]);
+    }
+
+    if(detail.length > 0) {
+        var filterEntry = addOutputWithAttributes(filterList, "li", ["class", "data-accordion-item"], ["accordion-item ", ""]);
+        var filterEntryHref = addOutputWithAttributes(filterEntry, "a", ["class", "href", "role", "id", "aria-expanded", "aria-selected", "aria-controls"], ["accordion-title js-anchor-target", "#detail_details", "tab", "detail_details-accordion-label", "false", "false", "detail_details-accordion"]);
+        addOutput(filterEntryHref, "span", "Details");
+        var filterEntrySub = addOutputWithAttributes(filterEntry, "div", ["class", "data-tab-content", "role", "id", "aria-hidden", "aria-labelledby"], ["accordion-content is-hidden", "", "tab", "detail_details-accordion", "true", "detail_details-accordion-label"]);
+        addOutputWithAttributes(filterEntrySub, "div", ["class"], ["boxes"]);
+    }
+}
+
+function addDetailTableRowWrapperNewLayout (parent, title, content) {
+    var result = addOutputWithAttributes(parent, "div", ["class"], ["table table--lined"]);
+    result = addOutput(result, "table", "");
+    result = addOutput(result, "tbody", "");
+    result = addOutput(result, "tr", "");
+    addOutput(result, "th", title);
+    result = addOutput(result, "td", "");
+    addOutput(result, "p", content);
+}
--- src/main/resources/mapping/wadaba-wfs-1.1.0_to_idf-1.0.0.js
+++ src/main/resources/mapping/wadaba-wfs-1.1.0_to_idf-1.0.0.js
@@ -57,40 +57,65 @@ var recordNode = wfsRecord.getOriginalResponse().get(0);
 //---------- <idf:body> ----------
 var idfBody = xPathUtils.getNode(document, "/idf:html/idf:body");
 
+var detail = addOutputWithAttributes(idfBody, "section", ["class", "id"], ["detail", "detail"]);
+
 // header
-var header = addDetailHeaderWrapper(idfBody);
+var header = addDetailHeaderWrapperNewLayout(detail);
 
-// add the title
-addOutput(header, "h1", getTitle(recordNode));
+// header back to search
+addDetailHeaderWrapperNewLayoutBackSearch(header);
 
-//add the summary
-addOutput(header, "p", getSummary(recordNode));
+// header title
+addDetailHeaderWrapperNewLayoutTitle(header, getTitle(recordNode));
 
+// detail content
+
+var detailNavContent = addOutputWithAttributes(detail, "section", ["class"], ["row nav-content search-filtered"]);
+
+// navigation
+addDetailHeaderWrapperNewLayoutDetailNavigation(detailNavContent, getSummary(recordNode), recordNode.getChildNodes())
+
+// content
+addOutputWithAttributes(detailNavContent, "a", ["class", "id"], ["anchor", "detail_overview"]);
+
+detailNavContent = addOutputWithAttributes(detailNavContent, "div", ["class"], ["xsmall-24 large-18 xlarge-18 columns"]);
+
+var detailNavContentData = addOutputWithAttributes(detailNavContent, "div", ["class"], ["data"]);
+detailNavContentData = addOutputWithAttributes(detailNavContentData, "div", ["class"], ["teaser-data search row is-active"]);
+
+var detailNavContentDataLeft = addOutputWithAttributes(detailNavContentData, "div", ["class"], ["xsmall-24 small-24 medium-14 large-14 xlarge-14 columns"]);
 //add the bounding box
 var boundingBox = getBoundingBox(recordNode);
-addOutput(header, "h2", "Ort:");
-var coordList = addOutput(header, "ul");
-addOutput(coordList, "li", "Nord: "+boundingBox.y2);
-addOutput(coordList, "li", "West: "+boundingBox.x1);
-addOutput(coordList, "li", "Ost: "+boundingBox.x2);
-addOutput(coordList, "li", "S&uuml;d: "+boundingBox.y1);
+if(boundingBox)  {
+    addOutput(detailNavContentDataLeft, "h4", "Ort:");
+    addDetailTableRowWrapperNewLayout(detailNavContentDataLeft, "Nord", boundingBox.y2);
+    addDetailTableRowWrapperNewLayout(detailNavContentDataLeft, "West", boundingBox.x1);
+    addDetailTableRowWrapperNewLayout(detailNavContentDataLeft, "Ost", boundingBox.x2);
+    addDetailTableRowWrapperNewLayout(detailNavContentDataLeft, "S&uuml;d", boundingBox.y1);
+}
 
-// add the map preview
-addOutput(header, "div", getMapPreview(recordNode));
+if(getMapPreview(recordNode)) {
+    detailNavContentData.appendChild(document.createTextNode(getMapPreview(recordNode)));
+}
 
-// details
-var details = addDetailDetailsWrapper(idfBody);
+if(getSummary(recordNode)) {
+    var detailNavContentSection = addOutputWithAttributes(detailNavContent, "div", ["class"], ["section"]);
+    addOutputWithAttributes(detailNavContent, "a", ["class", "id"], ["anchor", "detail_description"]);
+    addOutput(detailNavContentDataRight, "h3", "Beschreibung");
+}
 
-// add details (content of all child nodes)
-addOutput(details, "h2", "Details:");
-var detailList = addOutput(details, "ul");
 var detailNodes = recordNode.getChildNodes();
-for (var i=0, count=detailNodes.length; i<count; i++) {
-	var detailNode = detailNodes.item(i);
-	var nodeName = detailNode.getLocalName();
-	if (hasValue(nodeName)) {
-		addOutputWithLinks(detailList, "li", detailNode.getLocalName()+": "+detailNode.getTextContent());
-	}
+if(detailNodes.length > 0) {
+    var detailNavContentSection = addOutputWithAttributes(detailNavContent, "div", ["class"], ["section"]);
+    addOutputWithAttributes(detailNavContent, "a", ["class", "id"], ["anchor", "detail_details"]);
+    addOutput(detailNavContent, "h3", "Details");
+    for (var i=0, count=detailNodes.length; i<count; i++) {
+        var detailNode = detailNodes.item(i);
+        var nodeName = detailNode.getLocalName();
+        if (hasValue(nodeName)) {
+            addDetailTableRowWrapperNewLayout(detailNavContent, detailNode.getLocalName(), detailNode.getTextContent());
+        }
+    }
 }
 
 // functions
@@ -134,7 +159,7 @@ function getMapPreview(recordNode) {
         var E = Number(upperCoords[1]); // EAST, x2
         // NOTICE: 
         // lowerCorner and upperCorner have same coordinates in Wadaba !? -> BBOX is a POINT !
-        var BBOX = "" + (E - 0.048) + "," + (S - 0.012) + "," + (E + 0.048) + "," + (S + 0.012);
+        var BBOX = "" + (E - 0.012) + "," + (S - 0.012) + "," + (E + 0.012) + "," + (S + 0.012);
 
         // transform "WGS 84 (EPSG:4326)" to "ETRS89 / UTM zone 32N (EPSG:25832)"
         var transfCoords = CoordTransformUtil.getInstance().transform(
@@ -145,16 +170,14 @@ function getMapPreview(recordNode) {
         var S_25832 = transfCoords[1];
 //        log.warn("transfCoords: IN(" + E + "," + S + "), OUT(" + E_25832 + "," + S_25832 + ")");
 
-        var addHtml = "<a href=\"/kartendienste?lang=de&topic=favoriten&bgLayer=wmts_topplus_web&layers=bwastr_vnetz&layers_opacity=0.4&E=" + E_25832 + "&N=" + S_25832 + "&zoom=15&crosshair=marker\" style=\"padding: 0 0 0 0;\">" +
-            "<div style=\"background-image: url(http://sgx.geodatenzentrum.de/wms_topplus_web_open?VERSION=1.3.0&amp;REQUEST=GetMap&amp;CRS=CRS:84&amp;BBOX=" + BBOX +
-            "&amp;LAYERS=web&amp;FORMAT=image/png&amp;STYLES=&amp;WIDTH=480&amp;HEIGHT=120); left: 0px; top: 0px; width: 480px; height: 120px; margin: 10px 0 0 0;\">" +
-            "<img src=\"/ingrid-portal-apps/images/map_punkt.png\" alt=\"\">" +
-            "</div></a>";
-
+        var addHtml = "<div class=\"xsmall-24 small-24 medium-10 columns\">";
+        addHtml += "<h4 class=\"text-center\">Vorschau</h4>";
+        addHtml += "<div class=\"swiper-container-background\"><div class=\"swiper-slide\"><div class=\"caption\"><div class=\"preview_image\">";
+        addHtml += "<iframe src=\"/ingrid-webmap-client/frontend/prd/embed.html?lang=de&zoom=15&topic=favoriten&bgLayer=wmts_topplus_web&layers=bwastr_vnetz&layers_opacity=0.4&E=" + E_25832 + "&N=" + S_25832 + "&crosshair=marker\" style=\"height:320px\"></iframe>";
+        addHtml += "</div></div></div></div></div>";
         if (log.isDebugEnabled()) {
             log.debug("MapPreview Html: " + addHtml);
         }
-
         return addHtml;
     }
 }
